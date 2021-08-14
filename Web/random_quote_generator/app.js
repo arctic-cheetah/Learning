d090b8ab953d098c828dbd14f7e83a42da47c294
arctic-cheetah <gondonlondon@gmail.com>
2021-08-14 20:35:20 +1000

    random quote generator

---
+++ Web/random_quote_generator/app.js
@@ -0,0 +1,8 @@
+//TO DO
+//Make the background colour strobe colours
+//
+
+const body = document.querySelector(".root");
+const share_box = document.querySelector("share");
+const 
+console.log(body);
