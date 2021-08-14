d987f60acc7d6dd0d667c6e124d79d5f302962a5
arctic-cheetah <gondonlondon@gmail.com>
2021-08-15 01:27:09 +1000

    random colour change implemented

--- Web/random_quote_generator/app.js
+++ Web/random_quote_generator/app.js
@@ -2,7 +2,36 @@
 //Make the background colour strobe colours
 //
 
+//Colour change
 const body = document.querySelector(".root");
-const share_box = document.querySelector("share");
-const 
-console.log(body);
+const change_quote = document.getElementById("new-quote");
+const quote_box = document.getElementById("quote-box");
+const share = document.querySelector(".share");
+//Change the colour of the required elements after a click
+function change_colour() {
+  let colour = [0, 0, 0, 0];
+  for (let i = 0; i < 4; i += 1) {
+    //Any RGB value is between 0 - 256
+    if (i < 3) {
+      colour[i] = Math.random() * 256;
+    }
+    //Alpha value
+    else {
+      colour[i] = Math.random();
+    }
+  }
+  //Convert array to string
+  let rgba_val = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]}, ${colour[3]})`;
+  let rgba_val_darker = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]}, ${
+    colour[3] + 1
+  })`;
+  //Apply colour to all style
+  body.style.backgroundColor = rgba_val;
+  change_quote.style.backgroundColor = rgba_val;
+
+  change_quote.style.border = `2px solid ${rgba_val}`;
+  share.firstElementChild.style.backgroundColor = rgba_val;
+  share.lastElementChild.style.backgroundColor = rgba_val;
+}
+
+change_quote.addEventListener("click", change_colour);
