4f7d3b871230ee10ce0e9cf6894ef1f17e0dcc0a
arctic-cheetah <gondonlondon@gmail.com>
2021-08-16 02:19:12 +1000

    problem with transition

--- Web/random_quote_generator/app.js
+++ Web/random_quote_generator/app.js
@@ -1,6 +1,6 @@
 //TO DO
-//Make the background colour strobe colours
-//
+//Add quote to twitter link
+//Add fade out to quote
 
 //Colour change
 const body = document.querySelector(".root");
@@ -10,6 +10,13 @@ const share = document.querySelector(".share");
 
 //Quote URL
 const URL_quote = "https://api.quotable.io/random";
+
+//Execution
+get_new_quote();
+setTimeout(transition, 500);
+
+///////////////////////////////////////////////////////
+//Function Defintions
 
 //Change the colour of the required elements after a click
 function change_colour() {
@@ -24,6 +31,7 @@ function change_colour() {
       colour[i] = Math.random();
     }
   }
+
   //Convert array to string
   let rgba_val = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]}, ${colour[3]})`;
   let rgba_val_darker = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]}, ${
@@ -32,23 +40,39 @@ function change_colour() {
   //Apply colour to all style
   body.style.backgroundColor = rgba_val;
   change_quote.style.backgroundColor = rgba_val;
-
   change_quote.style.border = `2px solid ${rgba_val}`;
   share.firstElementChild.style.backgroundColor = rgba_val;
   share.lastElementChild.style.backgroundColor = rgba_val;
 }
 
+function text_fadein() {
+  quote_box.children[1].style.opacity = 1;
+  quote_box.firstElementChild.style.opacity = 1;
+}
+
+function transition() {
+  quote_box.children[1].classList.toggle("fade");
+  quote_box.firstElementChild.classList.toggle("fade");
+}
+
 function get_new_quote() {
   fetch(URL_quote)
-    .then((response) => response.json())
+    .then((response) => {
+      if (200 <= response && response < 300) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
     .then((data) => {
-      console.log(data);
       quote_box.firstElementChild.innerText = data.content;
       quote_box.children[1].innerText = data.author;
+    })
+    .catch((e) => {
+      console.log("Error " + e.message);
     });
 }
 
 change_quote.addEventListener("click", () => {
-  change_colour();
   get_new_quote();
+  change_colour();
 });
